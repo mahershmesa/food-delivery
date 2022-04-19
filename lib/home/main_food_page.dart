5aa60ab9fdf6c9102de894d8fe168ba9203fbe52
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-04-19 01:53:07 -0700

    add file home main home

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({ Key? key }) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+     child: Container(), 
+    );
+  }
+}
