8205d1a8949e3f9651f1c49ae08b386bc7de7d7c
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-04-20 03:04:09 -0700

    first commit

--- lib/widgets/big_text.dart
+++ lib/widgets/big_text.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class BigText extends StatelessWidget {
-  final Color? color;
+  Color? color;
   final String text;
   double size;
   TextOverflow overflow;
   BigText({ Key? key,
-  this.color, required this.text,this.overflow=TextOverflow.ellipsis,this.size=20, }) : super(key: key);
+  this.color = const Color(0xFF332d2b), required this.text,this.overflow=TextOverflow.ellipsis,this.size=20, }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
