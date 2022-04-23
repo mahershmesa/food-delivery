d2a555c908cc0d42f75f2cae55eb09aae1b710bd
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-04-23 03:22:55 -0700

    first commit

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery/utils/colors.dart';
+import 'package:flutter_food_delivery/widgets/big_text.dart';
+import 'package:flutter_food_delivery/widgets/small_text.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({ Key? key }) : super(key: key);
@@ -8,24 +11,27 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+
+  PageController pageController=PageController(viewportFraction:0.85);
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.red,
       height: 320,
       child: PageView.builder(
-        itemCount: 5,
+        controller: pageController,
+        itemCount: 5, 
         itemBuilder: (context,position){
           return _buildPageItem(position);
         }),
     );
-  }
+  }//هلا هون بيخلص التابع 
   Widget _buildPageItem(int index){
     return Stack(
       children: [
         Container(
       height: 220,
-      margin:EdgeInsets.only(left: 5,right: 5),
+      margin:EdgeInsets.only(left: 10,right: 10),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(30),
         color:index.isEven?Color(0xFF69c5df):Color(0xFF9294cc),
@@ -39,12 +45,45 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     Align(
       alignment: Alignment.bottomCenter,
       child: Container(
-        height: 140,
-        margin:EdgeInsets.only(left: 40,right: 40,bottom: 15),
+        height: 120,
+        margin:EdgeInsets.only(left: 20,right: 20,bottom: 30),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(30),
           color:Colors.white,
         ),
+      child: Container(
+        padding: EdgeInsets.only(top: 15,left: 15,right: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            BigText(text: "Chinese Side"),
+            SizedBox(height: 10,),
+            Row(
+              children: [
+                Wrap(
+                  children: 
+                    List.generate(5, (index) => Icon(Icons.star,color: AppColors.mainColor,size: 15,),
+                    ),
+                  
+                ),
+                SizedBox(width: 10,),
+                SmallText(text: "4.5"),
+                SizedBox(width: 10,),
+                SmallText(text: "1287"),
+                SizedBox(width: 10,),
+                SmallText(text: "comments")
+              ],
+            ),
+            SizedBox(height: 20,),
+            Row(
+              children: [
+                
+              ],
+            ),
+          ],
+        ),
+      ),
+
       ),
     ),
       ],
