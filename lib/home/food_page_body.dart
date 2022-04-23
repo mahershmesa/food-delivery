18821af93cf50880b15cb85fac92b0e629994ec3
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-04-23 01:03:33 -0700

    first commit

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -11,6 +11,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.red,
       height: 320,
       child: PageView.builder(
         itemCount: 5,
@@ -20,16 +21,33 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     );
   }
   Widget _buildPageItem(int index){
-    return Container(
+    return Stack(
+      children: [
+        Container(
       height: 220,
       margin:EdgeInsets.only(left: 5,right: 5),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(30),
         color:index.isEven?Color(0xFF69c5df):Color(0xFF9294cc),
         image: DecorationImage(
+          fit: BoxFit.cover,
           image: AssetImage("assets/image/food1.png")
         )
       ),
+    ),
+    ////////////تداخل container
+    Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: 140,
+        margin:EdgeInsets.only(left: 40,right: 40,bottom: 15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color:Colors.white,
+        ),
+      ),
+    ),
+      ],
     );
   }
 }
