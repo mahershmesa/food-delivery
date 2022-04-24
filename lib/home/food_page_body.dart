edf6fe9ec4694bd3c148aef3fd5438e14a8b7f55
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-04-24 03:39:20 -0700

    add settranslation index in matrex

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -16,6 +16,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController=PageController(viewportFraction:0.85);
   var _currPageValue=0.0;
   double _scaleFactor=0.8;
+  double _height=220;
 @override
   void initState() {
     // TODO: implement initState
@@ -47,88 +48,94 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }//هلا هون بيخلص التابع 
   Widget _buildPageItem(int index){
 
-
-
     Matrix4 matrix = new Matrix4.identity();
     if(index==_currPageValue.floor()){
       var currScale = 1-(_currPageValue-index)*(1-_scaleFactor);
+      var currTrans =_height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
     }else if(index==_currPageValue.floor()+1){
       var currScale = _scaleFactor+(_currPageValue-index+1)*(1-_scaleFactor);
+      var currTrans =_height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
     }
 
 
 
-    return Stack(
-      children: [
-        Container(
-      height: 220,
-      margin:EdgeInsets.only(left: 10,right: 10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color:index.isEven?Color(0xFF69c5df):Color(0xFF9294cc),
-        image: DecorationImage(
-          fit: BoxFit.cover,
-          image: AssetImage("assets/image/food1.png")
-        )
-      ),
-    ),
-    ////////////تداخل container
-    Align(
-      alignment: Alignment.bottomCenter,
-      child: Container(
-        height: 120,
-        margin:EdgeInsets.only(left: 20,right: 20,bottom: 30),
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+        height: 220,
+        margin:EdgeInsets.only(left: 10,right: 10),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(30),
-          color:Colors.white,
+          color:index.isEven?Color(0xFF69c5df):Color(0xFF9294cc),
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage("assets/image/food1.png")
+          )
         ),
-      child: Container(
-        padding: EdgeInsets.only(top: 15,left: 15,right: 15),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            BigText(text: "Chinese Side"),
-            SizedBox(height: 10,),
-            Row(
-              children: [
-                Wrap(
-                  children: 
-                    List.generate(5, (index) => Icon(Icons.star,color: AppColors.mainColor,size: 15,),
+      ),
+      ////////////تداخل container
+      Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          height: 120,
+          margin:EdgeInsets.only(left: 20,right: 20,bottom: 30),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color:Colors.white,
+          ),
+        child: Container(
+          padding: EdgeInsets.only(top: 15,left: 15,right: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              BigText(text: "Chinese Side"),
+              SizedBox(height: 10,),
+              Row(
+                children: [
+                  Wrap(
+                    children: 
+                      List.generate(5, (index) => Icon(Icons.star,color: AppColors.mainColor,size: 15,),
+                      ),
+                    
+                  ),
+                  SizedBox(width: 10,),
+                  SmallText(text: "4.5"),
+                  SizedBox(width: 10,),
+                  SmallText(text: "1287"),
+                  SizedBox(width: 10,),
+                  SmallText(text: "comments")
+                ],
+              ),
+              SizedBox(height: 20,),
+              Row(
+                children: [
+                  IconAndTextWidget(icon: Icons.circle_sharp,
+                    text: "Normal",
+                    iconColor: AppColors.iconColor1
                     ),
-                  
-                ),
-                SizedBox(width: 10,),
-                SmallText(text: "4.5"),
-                SizedBox(width: 10,),
-                SmallText(text: "1287"),
-                SizedBox(width: 10,),
-                SmallText(text: "comments")
-              ],
-            ),
-            SizedBox(height: 20,),
-            Row(
-              children: [
-                IconAndTextWidget(icon: Icons.circle_sharp,
-                  text: "Normal",
-                  iconColor: AppColors.iconColor1
-                  ),
-                  IconAndTextWidget(icon: Icons.location_on,
-                  text: "1.7km",
-                  iconColor: AppColors.mainColor
-                  ),
-                  IconAndTextWidget(icon: Icons.access_time_rounded,
-                  text: "32min",
-                  iconColor: AppColors.iconColor2
-                  ),
-              ],
-            ),
-          ],
+                    IconAndTextWidget(icon: Icons.location_on,
+                    text: "1.7km",
+                    iconColor: AppColors.mainColor
+                    ),
+                    IconAndTextWidget(icon: Icons.access_time_rounded,
+                    text: "32min",
+                    iconColor: AppColors.iconColor2
+                    ),
+                ],
+              ),
+            ],
+          ),
+        ),
+    
         ),
       ),
-
+        ],
       ),
-    ),
-      ],
     );
   }
 }
