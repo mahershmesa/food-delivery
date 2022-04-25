f4f954535bb6d82ac34e2ae6c75dbcc64c04a9b7
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-04-25 02:16:45 -0700

    edit

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,6 +1,7 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery/utils/colors.dart';
+import 'package:flutter_food_delivery/utils/dimansions.dart';
 import 'package:flutter_food_delivery/widgets/big_text.dart';
 import 'package:flutter_food_delivery/widgets/icon_and_text_widget.dart';
 import 'package:flutter_food_delivery/widgets/small_text.dart';
@@ -17,7 +18,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController=PageController(viewportFraction:0.85);
   var _currPageValue=0.0;
   double _scaleFactor=0.8;
-  double _height=220;
+  double _height=Dimensions.pageViewContainer;
 @override
   void initState() {
     // TODO: implement initState
@@ -40,7 +41,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       children: [
         Container(
           //color: Colors.red,
-          height: 320,
+          height: Dimensions.pageView,
           child: PageView.builder(
             controller: pageController,
             itemCount: 5, 
@@ -90,7 +91,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       child: Stack(
         children: [
           Container(
-        height: 220,
+        height:Dimensions.pageViewContainer,
         margin:EdgeInsets.only(left: 10,right: 10),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(30),
@@ -105,7 +106,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       Align(
         alignment: Alignment.bottomCenter,
         child: Container(
-          height: 120,
+          height: Dimensions.pageViewTextContainer,
           margin:EdgeInsets.only(left: 20,right: 20,bottom: 30),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30),
@@ -144,6 +145,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
               ),
               SizedBox(height: 20,),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   IconAndTextWidget(icon: Icons.circle_sharp,
                     text: "Normal",
