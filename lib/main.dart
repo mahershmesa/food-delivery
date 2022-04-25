526f533e77d802159a0123060745fd9d54c4d98f
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-04-25 01:55:53 -0700

    add getx and const hieght and width

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery/home/main_food_page.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
---
+++ lib/utils/dimansions.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+class Dimensions{
+  static double screenHeight = Get.context!.height;
+  static double screenWidth = Get.context!.width;
+
+
+  static double pageViewContainer= screenHeight/3.84;
+  static double pageViewTextContainer= screenWidth/7.03;
+}
