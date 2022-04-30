49b5c76918588747243223d57323f6384d61db34
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-04-29 23:34:24 -0700

    edit pupular

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class PupularFoodDetail extends StatelessWidget {
   const PupularFoodDetail({ Key? key }) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            child:
+            Container(
+              
+            ),
+            ),
+        ],
+      ),
     );
   }
 }
