fa933b383de05e4263ad5eb37d43462f5f34f981
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-04-29 23:58:11 -0700

    edit pupular

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -10,9 +10,12 @@ class PupularFoodDetail extends StatelessWidget {
       body: Stack(
         children: [
           Positioned(
+            left: 0,
+            right: 0,
             child:
             Container(
-              
+            width: double.maxFinite,
+            height: 350,
             ),
             ),
         ],
