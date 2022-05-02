08600e67df6575199e796d5152bb28ea8394d288
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-05-02 04:33:41 -0700

    First commit

--- lib/utils/dimansions.dart
+++ lib/utils/dimansions.dart
@@ -23,8 +23,9 @@ class Dimensions{
   static double width15 = screenHeight/56.27;
   static double width30 = screenHeight/28.13;
 
-
+//font size
   static double font20 = screenHeight/42.2;
+  static double font26 = screenHeight/32.46;
 //radius
   static double radius20 = screenHeight/42.2;
   static double radius30 = screenHeight/28.13;
@@ -33,6 +34,7 @@ class Dimensions{
 
   //icon Size
   static double iconSize24 = screenHeight/35.17;
+  static double iconSize16 = screenHeight/52.75;
 
 
 
