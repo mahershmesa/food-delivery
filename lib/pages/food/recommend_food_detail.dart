e4233f865a84ba61561b2a0224d75463f60823aa
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-05-06 02:42:08 -0700

    edit file and add margin

--- lib/pages/food/recommend_food_detail.dart
+++ lib/pages/food/recommend_food_detail.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery/utils/colors.dart';
+import 'package:flutter_food_delivery/utils/dimansions.dart';
+import 'package:flutter_food_delivery/widgets/big_text.dart';
 
 class RecomenedFoodDetail extends StatelessWidget {
   const RecomenedFoodDetail({ Key? key }) : super(key: key);
@@ -10,13 +13,35 @@ class RecomenedFoodDetail extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(20),
+              child: Container(
+               // margin: EdgeInsets.only(left: Dimensions.width20,right: Dimensions.width20),
+                color: Colors.white,
+                child: Center(child: BigText(size: Dimensions.font26, text: "Chiinese SIDE")),
+                width: double.maxFinite,
+                padding: EdgeInsets.only(top: 5,bottom: 10),
+              ),
+            ),
+            pinned: true,
+            backgroundColor: AppColors.yellowColor,
+            expandedHeight: 300,
             flexibleSpace: FlexibleSpaceBar(
               background: Image.asset(
                 "assets/image/food0.png",
                 width: double.maxFinite,
+                fit: BoxFit.cover,
                 ),
             ),
-          )
+          ),
+        
+        SliverToBoxAdapter(
+          child: Text(
+            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+
+          ),
+        )
+
         ],
       ),
     );
