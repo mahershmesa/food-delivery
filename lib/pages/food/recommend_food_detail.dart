177bf24fa1d04eb6328656ef149efc77a91173c3
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-05-07 03:33:30 -0700

    first commit

--- lib/pages/food/recommend_food_detail.dart
+++ lib/pages/food/recommend_food_detail.dart
@@ -4,6 +4,7 @@ import 'package:flutter_food_delivery/utils/colors.dart';
 import 'package:flutter_food_delivery/utils/dimansions.dart';
 import 'package:flutter_food_delivery/widgets/app_icon.dart';
 import 'package:flutter_food_delivery/widgets/big_text.dart';
+import 'package:flutter_food_delivery/widgets/exandable_text_widget.dart';
 
 class RecomenedFoodDetail extends StatelessWidget {
   const RecomenedFoodDetail({ Key? key }) : super(key: key);
@@ -11,6 +12,7 @@ class RecomenedFoodDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
@@ -26,10 +28,17 @@ class RecomenedFoodDetail extends StatelessWidget {
               preferredSize: Size.fromHeight(20),
               child: Container(
                // margin: EdgeInsets.only(left: Dimensions.width20,right: Dimensions.width20),
-                color: Colors.white,
+              
                 child: Center(child: BigText(size: Dimensions.font26, text: "Chiinese SIDE")),
                 width: double.maxFinite,
                 padding: EdgeInsets.only(top: 5,bottom: 10),
+                decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(Dimensions.radius20),
+                  topRight: Radius.circular(Dimensions.radius20),
+                )
+                ),
               ),
             ),
             pinned: true,
@@ -45,14 +54,30 @@ class RecomenedFoodDetail extends StatelessWidget {
           ),
         
         SliverToBoxAdapter(
-          child: Text(
-            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-
-          ),
+          child: Column(
+            children: [
+              Container(
+                child: ExpandableTextWidget(text: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",),
+                margin: EdgeInsets.only(left: Dimensions.width20,right: Dimensions.width20),
+                    ),
+            ],
+          )
         )
 
         ],
       ),
+    bottomNavigationBar: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          AppIcon( icon: Icons.remove),
+          AppIcon(icon: Icons.add),
+        ],
+      ),
+      ],
+    ),
     );
   }
 }
