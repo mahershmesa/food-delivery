c97be80aeee717f588b501557a1982f7c7f4a248
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-05-07 03:41:57 -0700

    end page recommend food detail

--- lib/pages/food/recommend_food_detail.dart
+++ lib/pages/food/recommend_food_detail.dart
@@ -69,12 +69,61 @@ class RecomenedFoodDetail extends StatelessWidget {
     bottomNavigationBar: Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          AppIcon( icon: Icons.remove),
-          AppIcon(icon: Icons.add),
-        ],
+      Container(
+        padding: EdgeInsets.only(
+          left: Dimensions.width20*2.5,
+          right: Dimensions.width20*2.5,
+          top: Dimensions.height10,
+          bottom: Dimensions.height10,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            AppIcon(iconSize: Dimensions.iconSize24, iconColor: Colors.white, backgroundColor: AppColors.mainColor, icon: Icons.remove),
+            BigText(text: "\$12.88"+"x"+"0",color: AppColors.mainBlackColor,size: Dimensions.font16,),
+            AppIcon(iconSize: Dimensions.iconSize24, iconColor: Colors.white, backgroundColor: AppColors.mainColor, icon: Icons.add),
+          ],
+        ),
+      ),
+      ///////////////////////////pupularنسخنا نفس الي بل
+      Container(
+        height: Dimensions.bottomheightbar,
+        padding: EdgeInsets.only(top: Dimensions.height30,bottom: Dimensions.height30,left: Dimensions.width20,right: Dimensions.width20),
+        decoration: BoxDecoration(
+          color: AppColors.buttonBackgroundColor,
+          borderRadius: BorderRadius.only(
+            
+            topLeft: Radius.circular(Dimensions.radius20*2),
+            topRight: Radius.circular(Dimensions.radius20*2),
+          )
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: Dimensions.height20,bottom: Dimensions.height20,left: Dimensions.width20,right: Dimensions.width20),
+            
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Dimensions.radius20),
+                color: Colors.white,
+              ),
+              child: Icon(
+                Icons.favorite,
+                color: AppColors.mainColor,
+              ),
+            ),
+            ///////
+            Container(
+              padding: EdgeInsets.only(top: Dimensions.height20,bottom: Dimensions.height20,left: Dimensions.width20,right: Dimensions.width20),
+
+              child: BigText(text: "\$10 add to cart",color: Colors.white),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Dimensions.radius20),
+                color: AppColors.mainColor,
+              ),
+            ),
+          ],
+        ),
       ),
       ],
     ),
