c210af85ff32bb31123d5f998640cc320c11130c
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-05-09 01:30:58 -0700

    app baseurl

--- lib/data/api/api_client.dart
+++ lib/data/api/api_client.dart
@@ -2,4 +2,14 @@ import 'package:get/get.dart';
 
 class ApiClient extends GetConnect implements GetxService{
   late String token;
+  final String AppBaseUrl;
+
+  late Map<String,String> _mainHeaders;
+  ApiClient({
+    required this.AppBaseUrl
+  }){
+    baseUrl = AppBaseUrl;
+    timeout = Duration(seconds: 30);
+  }
+
 }
