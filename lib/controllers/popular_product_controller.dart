47a94eff71728d5235d5f5273d01cb990ed458bc
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-05-10 01:31:43 -0700

    init popular conteroller

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -1,5 +1,7 @@
+import 'package:flutter_food_delivery/data/repository/popular_product_repo.dart';
 import 'package:get/get.dart';
 
 class PopularProductController extends GetxController{
-  
+  final PopularProductRepo popularProductRepo;
+  PopularProductController({required this.popularProductRepo});
 }
