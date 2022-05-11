41075e49218c90c10213c4d4b1ff99bb8b19fc61
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-05-10 23:15:16 -0700

    layzbut in dependencis

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,3 +1,7 @@
-Future<void> init()async{
+import 'package:flutter_food_delivery/data/api/api_client.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/src/extension_instance.dart';
 
+Future<void> init()async{
+Get.lazyPut(()=>ApiClient(AppBaseUrl: "https://www.dbestech.com"));
 }
