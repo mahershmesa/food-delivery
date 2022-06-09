05c522deb932115f3650bd6e7cc35b776d70e058
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-06-09 04:01:50 -0700

    first commit

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -1,22 +1,22 @@
-import 'package:flutter_food_delivery/data/repository/popular_product_repo.dart';
-import 'package:flutter_food_delivery/model/products_model.dart';
-import 'package:get/get.dart';
+// import 'package:flutter_food_delivery/data/repository/popular_product_repo.dart';
+// import 'package:flutter_food_delivery/model/products_model.dart';
+// import 'package:get/get.dart';
 
-class PopularProductController extends GetxController{
-  final PopularProductRepo popularProductRepo;
-  PopularProductController({required this.popularProductRepo});
-  List<dynamic> _popularProductList=[];
-  List<dynamic> get popularProductList => _popularProductList;
+// class PopularProductController extends GetxController{
+//   final PopularProductRepo popularProductRepo;
+//   PopularProductController({required this.popularProductRepo});
+//   List<dynamic> _popularProductList=[];
+//   List<dynamic> get popularProductList => _popularProductList;
 
 
-  Future<void> getPopularProductList()async{
-    Response response = await popularProductRepo.getPopularProductList();
-    if(response.statusCode==200){
-      _popularProductList=[];
-      _popularProductList.addAll(Product.fromJson(response.body).products);
-      update();
-    }else{
+//   Future<void> getPopularProductList()async{
+//     Response response = await popularProductRepo.getPopularProductList();
+//     if(response.statusCode==200){
+//       _popularProductList=[];
+//       _popularProductList.addAll(Product.fromJson(response.body).products);
+//       update();
+//     }else{
 
-    }
-  }
-}
+//     }
+//   }
+// }
