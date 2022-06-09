9ce9d49ff7b2e52b329691fdb345e5bede2051d3
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-06-09 03:30:45 -0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery/controllers/popular_product_controller.dart';
 import 'package:flutter_food_delivery/pages/food/popular_food_detail.dart';
 //import 'package:flutter_food_delivery/home/main_food_page.dart';
 import 'package:flutter_food_delivery/pages/home/main_food_page.dart';
+import 'package:get/get.dart';
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'helper/dependencies.dart' as dep;
@@ -15,6 +17,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    Get.find<PopularProductController>().getPopularProductList();
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
