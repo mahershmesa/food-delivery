ffc7b8ff8f2ce580092e5bf0aa6265998ff3d962
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-07-05 20:29:36 -0700

    add api to conect

--- lib/data/repository/popular_product_repo.dart
+++ lib/data/repository/popular_product_repo.dart
@@ -1,12 +1,16 @@
 import 'package:flutter_food_delivery/data/api/api_client.dart';
 import 'package:flutter_food_delivery/utils/app_constants.dart';
+import 'package:food_delivery_application/data/api/api_client.dart';
+import 'package:food_delivery_application/utils/app_constants.dart';
 import 'package:get/get.dart';
 
 class PopularProductRepo extends GetxService{
   final ApiClient apiClient;
+
   PopularProductRepo({required this.apiClient});
 
-  Future<Response> getPopularProductList()async{
-    return await apiClient.getData(AppConstants.POPULAR_PRODUCT_URL);
+  Future<Response> getPopularProductList()async
+  {
+    return await apiClient.getData(AppConstants.POPULAR_PRODUCT_URI);
   }
 }
