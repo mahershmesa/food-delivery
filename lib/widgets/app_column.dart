79b632542dc977b834492f901b4935ee36e2c97e
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-07-08 17:33:49 -0700

    add multi file and edit

--- lib/widgets/app_column.dart
+++ lib/widgets/app_column.dart
@@ -1,58 +1,59 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_food_delivery/utils/colors.dart';
-import 'package:flutter_food_delivery/utils/dimansions.dart';
-import 'package:flutter_food_delivery/widgets/small_text.dart';
+import 'package:food_delivery_application/widgets/small_text.dart';
 
+import '../utils/colors.dart';
+import '../utils/dimensions.dart';
 import 'big_text.dart';
 import 'icon_and_text_widget.dart';
 
 class AppColumn extends StatelessWidget {
   final String text;
-  const AppColumn({ Key? key , required this.text}) : super(key: key);
+
+  const AppColumn({Key? key,required this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              BigText(text: text,size: Dimensions.font26,),
-              SizedBox(height: Dimensions.height10,),
-              Row(
-                children: [
-                  Wrap(
-                    children: 
-                      List.generate(5, (index) => Icon(Icons.star,color: AppColors.mainColor,size: 15,),
-                      ),
-                    
-                  ),
-                  SizedBox(width: 10,),
-                  SmallText(text: "4.5"),
-                  SizedBox(width: 10,),
-                  SmallText(text: "1287"),
-                  SizedBox(width: 10,),
-                  SmallText(text: "comments")
-                ],
-              ),
-              SizedBox(height: Dimensions.height20,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconAndTextWidget(icon: Icons.circle_sharp,
-                    text: "Normal",
-                    iconColor: AppColors.iconColor1
-                    ),
-                    IconAndTextWidget(icon: Icons.location_on,
-                    text: "1.7km",
-                    iconColor: AppColors.mainColor
-                    ),
-                    IconAndTextWidget(icon: Icons.access_time_rounded,
-                    text: "32min",
-                    iconColor: AppColors.iconColor2
-                    ),
-                ],
-              ),
-            ],
-          );
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        BigText(text: text,size: Dimensions.font26,),
+        SizedBox(height: Dimensions.height10,),
+        Row(
+          children: [
+            Wrap(
+              children: List.generate(5, (index) =>Icon(Icons.star,color: AppColors.mainColor,size: 15,)),
+            ),
+            SizedBox(width: Dimensions.width10,),
+            SmallText(text: '4.5'),
+            SizedBox(width: Dimensions.width10,),
+            SmallText(text: '1287'),
+            SizedBox(width: Dimensions.width10,),
+            SmallText(text: 'comments')
+          ],
+        ),
+        SizedBox(height: Dimensions.height10*2),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconAndTextWidget(
+              icon: Icons.circle_sharp,
+              text: 'Normal',
+              iconColor: AppColors.iconColor1,
+            ),
+            IconAndTextWidget(
+              icon: Icons.location_on,
+              text: '1.7Km',
+              iconColor: AppColors.mainColor,
+            ),
+            IconAndTextWidget(
+              icon: Icons.access_time_rounded,
+              text: '32min',
+              iconColor: AppColors.iconColor2,
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
